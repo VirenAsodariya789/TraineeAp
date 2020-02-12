1831126301a18a7679eb4fd68ab5aed83e8988c9
Viren Asodariya <sc@SCs-Mac-mini.local>
2020-02-12 11:48:30 +0530

    state changed clicking state changed  example2

--- App.js
+++ App.js
@@ -9,38 +9,16 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
 
-class Blink extends Component {
-  componentDidMount() {
-    // Toggle the state every second
-    setInterval(
-      () =>
-        this.setState(previousState => ({
-          isShowingText: !previousState.isShowingText
-        })),
-      1000
-    );
-  }
-
-  //state object
-  state = { isShowingText: true };
-
-  render() {
-    if (!this.state.isShowingText) {
-      return null;
-    }
-
-    return <Text>{this.props.text}</Text>;
-  }
-}
-
-export default class BlinkApp extends Component {
+export default class App extends Component {
+  state = {
+    myState:
+      "This is a text component, created using state data. It will change or updated on clicking it."
+  };
+  updateState = () => this.setState({ myState: "The state is updated" });
   render() {
     return (
       <View>
-        <Blink text="I love to blink" />
-        <Blink text="Yes blinking is so great" />
-        <Blink text="Why did they ever take this out of HTML" />
-        <Blink text="Look at me look at me look at me" />
+        <Text onPress={this.updateState}> {this.state.myState} </Text>
       </View>
     );
   }
