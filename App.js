ff693e952166573d5ba6300322376a1eac4821bc
Viren Asodariya <sc@SCs-Mac-mini.local>
2020-02-12 10:54:41 +0530

    props using image changed

--- App.js
+++ App.js
@@ -6,17 +6,15 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { Image } from 'react-native';
+import React, { Component } from "react";
+import { Image } from "react-native";
 
 export default class Bananas extends Component {
   render() {
     let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+      uri:
+        "https://images.pexels.com/photos/1804035/pexels-photo-1804035.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
     };
-    return (
-      <Image source={pic} style={{width: 200, height: 150}}/>
-    );
+    return <Image source={pic} style={{ width: 200, height: 150 }} />;
   }
 }
-
