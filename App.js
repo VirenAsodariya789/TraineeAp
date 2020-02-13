262b50142950f1b4337a2bc19154df39feeb7474
Viren Asodariya <sc@SCs-Mac-mini.local>
2020-02-13 11:09:17 +0530

    height and width in Fixed dimensions example

--- App.js
+++ App.js
@@ -7,27 +7,17 @@
  */
 
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
-const styles = StyleSheet.create({
-  bigBlue: {
-    color: "blue",
-    fontWeight: "bold",
-    fontSize: 30
-  },
-  red: {
-    color: "red"
-  }
-});
-
-export default class LotsOfStyles extends Component {
+export default class FixedDimensionsBasic extends Component {
   render() {
     return (
       <View>
-        <Text style={styles.red}>Just red</Text>
-        <Text style={styles.bigBlue}>just bigblue</Text>
-        <Text style={[styles.bigBlue, styles.red]}>bigBlue,then red</Text>
-        <Text style={[styles.red, styles.bigBlue]}>red,then bigBlue</Text>
+        <View style={{ width: 50, height: 50, backgroundColor: "#f44336" }} />
+        <View style={{ width: 100, height: 100, backgroundColor: "#9c27b0" }} />
+        <View style={{ width: 150, height: 150, backgroundColor: "#009688" }} />
+        <View style={{ width: 200, height: 200, backgroundColor: "#8bc34a" }} />
+        <View style={{ width: 300, height: 300, backgroundColor: "#541777" }} />
       </View>
     );
   }
