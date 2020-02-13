c30525796e2ce6fbbb4d3cc3a8816cd845b6426b
Viren Asodariya <sc@SCs-Mac-mini.local>
2020-02-13 11:28:08 +0530

    height and width in style  Flex dimensions example

--- App.js
+++ App.js
@@ -9,15 +9,15 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
 
-export default class FixedDimensionsBasic extends Component {
+export default class FlexDimensionsBasic extends Component {
   render() {
     return (
-      <View>
-        <View style={{ width: 50, height: 50, backgroundColor: "#f44336" }} />
-        <View style={{ width: 100, height: 100, backgroundColor: "#9c27b0" }} />
-        <View style={{ width: 150, height: 150, backgroundColor: "#009688" }} />
-        <View style={{ width: 200, height: 200, backgroundColor: "#8bc34a" }} />
-        <View style={{ width: 300, height: 300, backgroundColor: "#541777" }} />
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1, backgroundColor: "red" }} />
+        <View style={{ flex: 2, backgroundColor: "green" }} />
+        <View style={{ flex: 3, backgroundColor: "blue" }} />
+        <View style={{ flex: 4, backgroundColor: "pink" }} />
+        <View style={{ flex: 5, backgroundColor: "yellow" }} />
       </View>
     );
   }
