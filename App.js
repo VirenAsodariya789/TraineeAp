3ccb2813b41a5df079580f69b2461f36e91cf169
Viren Asodariya <sc@SCs-Mac-mini.local>
2020-02-14 12:05:10 +0530

    Layout with flex,flexdirection,  justifycontent start,end,center,space-betwwen,space-around exmple

--- App.js
+++ App.js
@@ -9,10 +9,16 @@
 import React, { Component } from "react";
 import { View } from "react-native";
 
-export default class FlexDirectionBasic extends Component {
+export default class JustifyContentBasics extends Component {
   render() {
     return (
-      <View style={{ flex: 1, flexDirection: "column-reverse" }}>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          justifyContent: "space-around"
+        }}
+      >
         <View style={{ width: 50, height: 50, backgroundColor: "red" }} />
         <View style={{ width: 50, height: 50, backgroundColor: "green" }} />
         <View style={{ width: 50, height: 50, backgroundColor: "blue" }} />
