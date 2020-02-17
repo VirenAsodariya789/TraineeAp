ffd299adb3d951943547f56dae8dadc50b7406bf
Viren Asodariya <sc@SCs-Mac-mini.local>
2020-02-17 10:42:09 +0530

    align item all proprties strech start end center

--- App.js
+++ App.js
@@ -5,24 +5,26 @@
  * @format
  * @flow
  */
-
 import React, { Component } from "react";
 import { View } from "react-native";
 
-export default class JustifyContentBasics extends Component {
+export default class AlignItemsBasics extends Component {
   render() {
     return (
+      // Try setting `alignItems` to 'flex-start'
+      // Try setting `justifyContent` to `flex-end`.
+      // Try setting `flexDirection` to `row`.
       <View
         style={{
           flex: 1,
-          flexDirection: "row",
-          justifyContent: "space-around"
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "stretch"
         }}
       >
-        <View style={{ width: 50, height: 50, backgroundColor: "red" }} />
-        <View style={{ width: 50, height: 50, backgroundColor: "green" }} />
-        <View style={{ width: 50, height: 50, backgroundColor: "blue" }} />
-        <View style={{ width: 50, height: 50, backgroundColor: "pink" }} />
+        <View style={{ height: 50, backgroundColor: "red" }} />
+        <View style={{ height: 50, backgroundColor: "green" }} />
+        <View style={{ height: 100, backgroundColor: "blue" }} />
       </View>
     );
   }
