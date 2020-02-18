5785294390c70e5f542bdf21447e626576818410
Viren Asodariya <sc@SCs-Mac-mini.local>
2020-02-18 10:34:05 +0530

    flexbox in alignself properties and example

--- App.js
+++ App.js
@@ -8,23 +8,40 @@
 import React, { Component } from "react";
 import { View } from "react-native";
 
-export default class AlignItemsBasics extends Component {
+export default class AlignSelfDemo extends Component {
   render() {
     return (
-      // Try setting `alignItems` to 'flex-start'
-      // Try setting `justifyContent` to `flex-end`.
-      // Try setting `flexDirection` to `row`.
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "stretch"
-        }}
-      >
-        <View style={{ height: 50, backgroundColor: "red" }} />
-        <View style={{ height: 50, backgroundColor: "green" }} />
-        <View style={{ height: 100, backgroundColor: "blue" }} />
+      // Align Self Demo
+      // Properties 'flex-start'
+      //             'flex-end'
+      //              'center'
+      //              'stretch'
+
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <View
+          style={{
+            height: 50,
+            width: 50,
+            backgroundColor: "red"
+          }}
+        />
+        <View style={{ height: 50, width: 100, backgroundColor: "black" }} />
+        <View
+          style={{
+            height: 50,
+            width: 50,
+            alignSelf: "flex-end",
+            backgroundColor: "green"
+          }}
+        />
+        <View
+          style={{
+            height: 50,
+            width: 50,
+            alignSelf: "center",
+            backgroundColor: "blue"
+          }}
+        />
       </View>
     );
   }
