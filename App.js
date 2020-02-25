e8892ed446dbb9f0dd7fb23d0ab5769d89e91363
viren <asodariyaviren789@gmail.com>
2020-02-25 21:50:20 +0530

    Accordian list Example Demo

--- App.js
+++ App.js
@@ -6,36 +6,140 @@
  * @flow
  */
 
-import React, { Component } from "react";
-import { Image, Text, View } from "react-native";
-
-export default class App extends Component {
-  render() {
-    //React native card view example
-
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity,Image,Dimensions,ScrollView } from 'react-native';
+import SimpleAccordion from 'react-native-simple-accordian';
+const deviceWidth = Dimensions.get('window').width
+const accordianData = 'This is sample Accordian Text....';
+import * as _ from 'lodash'
+ 
+const sampleAccordianData = [
+  {
+    title: 'Accordian 1 ',
+    content: accordianData
+  },
+  {
+    title: 'Accordian 2',
+    content: accordianData
+  },
+  {
+    title: 'Accordian 3',
+    content: accordianData
+  },
+  {
+    title: 'Accordian 4 ',
+    content: accordianData
+  },
+  {
+    title: 'Accordian 5',
+    content: accordianData
+  },
+  {
+    title: 'Accordian 6',
+    content: accordianData
+  },
+  
+];
+ 
+export default class AccordianExample extends Component {
+    constructor(props){
+        super(props)
+        this.state = ({
+            content:'',
+            open: false,
+        });
+    }
+ 
+  onChangeAccordian(section) {
+    this.setState({ open: section });
+ 
+  }
+ 
+  renderHeader(section, i, isOpen) {
     return (
-      <View
-        style={{ width: 200, height: 200, flexDirection: "row", margin: 24 }}
-      >
-        <Image
-          style={{ width: 200, height: 200, position: "absolute" }}
-          source={{
-            uri: "https://www.dike.lib.ia.us/images/sample-1.jpg/image"
-          }}
-        />
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: "rgba(0, 0, 0, 0.5)",
-            alignSelf: "flex-end"
-          }}
-        >
-          <Text style={{ color: "white", fontSize: 20, margin: 6 }}>
-            {"Title"}
-          </Text>
-          <Text style={{ color: "white", margin: 6 }}>{"Subtitle"}</Text>
-        </View>
+      <View style={{backgroundColor:'#ffffff',flexDirection:'row'}}>
+        <Text style={[styles.headerText,{width:deviceWidth-80,padding:10,textAlign:'left'}]}>{section.title}</Text>
       </View>
     );
   }
+ 
+  renderContent(section, i, isOpen) {
+    return (
+      <View style={styles.content}>
+        <Text>{section.content}</Text>
+      </View>
+    );
+  }
+ 
+  render() {
+    return (
+      <View style={{flex: 1,}}>
+        <View style={styles.headerTextView}>
+          <Text style={styles.headerText1}>Accordian</Text>
+        </View>
+        <View style={styles.container}>
+          <ScrollView >    
+            <SimpleAccordion
+              style= {{
+                borderWidth:1,
+                borderRadius:25,
+                margin:10,
+                padding:10,
+                backgroundColor:'#ffffff'
+              }}
+              activeSection={this.state.open}
+              sections={sampleAccordianData}
+              touchableComponent={TouchableOpacity}
+              renderHeader={this.renderHeader}
+              renderContent={this.renderContent}
+              duration={1000}
+              onChange={this.onChangeAccordian.bind(this)}
+            />
+          </ScrollView>
+        </View>
+      </View>
+      
+    );
+  }
 }
+ 
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor:'#2c2c2c'
+      
+    },
+    headerText1:{
+      color:'white',
+      textAlign:'center',
+      fontSize:22
+     
+    },
+    headerTextView:{
+      height:50,
+      borderWidth:1,
+      backgroundColor:'#383636',
+      justifyContent:'center',
+    },
+    title: {
+      textAlign: 'center',
+      fontSize: 22,
+      fontWeight: '300',
+      marginBottom: 20,
+      color:'#ffffff'
+    },
+    header:{
+        flex:1
+    },
+    headerText: {
+      textAlign: 'center',
+      fontSize: 16,
+      fontWeight: '500',
+    },
+    content: {
+      padding: 20,
+      backgroundColor: '#ffffff'
+    },
+  });
+    
