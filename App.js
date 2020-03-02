c78de5ae1118d122ad668ac6f02d3c902fe03aa3
Viren Asodariya <sc@SCs-Mac-mini.local>
2020-03-02 11:35:39 +0530

    segment demo example22

--- App.js
+++ App.js
@@ -1,48 +1,21 @@
 /*This is an example of Segmented Control Tab in React Native*/
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //Import React
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 //Import Basic React Native Component
 
-import SegmentedControlTab from 'react-native-segmented-control-tab';
+import SegmentedControlTab from "react-native-segmented-control-tab";
 //Import SegmentedControlTab
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      selectedIndex: 0,
-      //Default selected Tab Index for single select SegmentedControlTab
-      selectedIndices: [0],
-      //Default selected Tab Indexes for multi select SegmentedControlTab
-      customStyleIndex: 0,
+      customStyleIndex: 0
       //Default selected Tab Indexes for cusatom SegmentedControlTab
     };
   }
-
-  handleSingleIndexSelect = (index: number) => {
-    //handle tab selection for single Tab Selection SegmentedControlTab
-    this.setState(prevState => ({ ...prevState, selectedIndex: index }));
-  };
-
-  handleMultipleIndexSelect = (index: number) => {
-    //handle tab selection for multi Tab Selection SegmentedControlTab
-    const { selectedIndices } = this.state;
-    if (selectedIndices.includes(index)) {
-      //if included in the selected array then remove
-      this.setState(prevState => ({
-        ...prevState,
-        selectedIndices: selectedIndices.filter(i => i !== index),
-      }));
-    } else {
-      //if not included in the selected array then add
-      this.setState(prevState => ({
-        ...prevState,
-        selectedIndices: [...selectedIndices, index],
-      }));
-    }
-  };
 
   handleCustomIndexSelect = (index: number) => {
     //handle tab selection for custom Tab Selection SegmentedControlTab
@@ -50,73 +23,38 @@ class App extends Component {
   };
 
   render() {
-    const { selectedIndex, selectedIndices, customStyleIndex } = this.state;
+    const { customStyleIndex } = this.state;
     return (
       <View style={styles.container}>
-        {/* Simple Segmented Control*/}
-        <Text style={styles.headerText}>
-          Simple Segmented Control with Single Selection
-        </Text>
-        <SegmentedControlTab
-          values={['Segment One', 'Segment two']}
-          selectedIndex={selectedIndex}
-          tabStyle={styles.tabStyle}
-          activeTabStyle={styles.activeTabStyle}
-          onTabPress={this.handleSingleIndexSelect}
-        />
-        <View style={styles.Seperator} />
-
-        {/* Additional badges in Simple Segmented Control*/}
-        <Text style={styles.headerText}>
-          Simple Segmented Control with Single Selection + Badges
-        </Text>
-        <SegmentedControlTab
-          badges={[12, 24]}
-          values={['Segment One', 'Segment two']}
-          selectedIndex={selectedIndex}
-          onTabPress={this.handleSingleIndexSelect}
-        />
-        <View style={styles.Seperator} />
-
-        {/* Simple Segmented Control with multi Select*/}
-        <Text style={styles.headerText}>
-          Simple Segmented Control with Multiple Selection
-        </Text>
-        <SegmentedControlTab
-          values={['Segment One', 'Segment two', 'Segment Three']}
-          multiple
-          //You need to add the multiple as conpared to single select
-          selectedIndices={selectedIndices}
-          //pass the selected index array for the default selection
-          onTabPress={this.handleMultipleIndexSelect}
-          //Pushing the selected option index in selected item array
-        />
-        <View style={styles.Seperator} />
         <Text style={styles.headerText}>
           Custom segmented control with custom styles
         </Text>
+        <View style={styles.Seperator} />
 
         {/* Simple Segmented with Custom Styling*/}
         <SegmentedControlTab
-          values={['one', 'two']}
+          values={["one", "two", "three"]}
           selectedIndex={customStyleIndex}
           onTabPress={this.handleCustomIndexSelect}
           borderRadius={0}
-          tabsContainerStyle={{ height: 50, backgroundColor: '#F2F2F2' }}
+          tabsContainerStyle={{ height: 50, backgroundColor: "#F2F2F2" }}
           tabStyle={{
-            backgroundColor: '#F2F2F2',
+            backgroundColor: "#F2F2F2",
             borderWidth: 0,
-            borderColor: 'transparent',
+            borderColor: "transparent"
           }}
-          activeTabStyle={{ backgroundColor: 'white', marginTop: 2 }}
-          tabTextStyle={{ color: '#444444', fontWeight: 'bold' }}
-          activeTabTextStyle={{ color: '#888888' }}
+          activeTabStyle={{ backgroundColor: "pink", marginTop: 2 }}
+          tabTextStyle={{ color: "navyblue", fontWeight: "bold" }}
+          activeTabTextStyle={{ color: "#888888" }}
         />
         {customStyleIndex === 0 && (
-          <Text style={styles.tabContent}> Tab one</Text>
+          <Text style={styles.tabContent}> Tab one Pressed</Text>
         )}
         {customStyleIndex === 1 && (
-          <Text style={styles.tabContent}> Tab two</Text>
+          <Text style={styles.tabContent}> Tab two Pressed</Text>
+        )}
+        {customStyleIndex === 2 && (
+          <Text style={styles.tabContent}> Tab three Pressed</Text>
         )}
       </View>
     );
@@ -126,35 +64,37 @@ class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    padding: 10,
+
+    alignItems: "center",
+    backgroundColor: "green",
+
+    padding: 10
   },
   headerText: {
     padding: 8,
-    fontSize: 14,
-    color: '#444444',
-    textAlign: 'center',
+    fontSize: 17,
+    color: "blue",
+    textAlign: "center"
   },
   tabContent: {
-    color: '#444444',
-    fontSize: 18,
-    margin: 24,
+    color: "red",
+    fontSize: 20,
+    margin: 24
   },
   Seperator: {
     marginHorizontal: -10,
-    alignSelf: 'stretch',
+    alignSelf: "stretch",
     borderTopWidth: 1,
-    borderTopColor: '#888888',
-    marginTop: 24,
+    borderTopColor: "black",
+    margin: 15,
+    marginTop: 24
   },
   tabStyle: {
-    borderColor: '#D52C43',
+    borderColor: "#D52C43"
   },
   activeTabStyle: {
-    backgroundColor: '#D52C43',
-  },
+    backgroundColor: "#D52C43"
+  }
 });
 
 export default App;
