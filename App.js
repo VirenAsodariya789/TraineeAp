1740b7a84657cb52faf0d02414caa2d1aeca6482
Viren Asodariya <sc@SCs-Mac-mini.local>
2020-03-02 11:51:25 +0530

    spinner simple example

--- App.js
+++ App.js
@@ -2,60 +2,39 @@
 import React, { Component } from "react";
 //Import React
 
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 //Import Basic React Native Component
 
-import SegmentedControlTab from "react-native-segmented-control-tab";
-//Import SegmentedControlTab
+import Spinner from "react-native-loading-spinner-overlay";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      customStyleIndex: 0
-      //Default selected Tab Indexes for cusatom SegmentedControlTab
-    };
-  }
-
-  handleCustomIndexSelect = (index: number) => {
-    //handle tab selection for custom Tab Selection SegmentedControlTab
-    this.setState(prevState => ({ ...prevState, customStyleIndex: index }));
+export default class App extends React.Component {
+  state = {
+    //default loading false
+    loading: false
   };
-
+  componentDidMount() {
+    //Setting a timer to show the spinner demo in every 3 second
+    setInterval(() => {
+      this.setState({
+        //change the state of the laoding in every 3 second
+        loading: !this.state.loading
+      });
+    }, 5000);
+  }
   render() {
-    const { customStyleIndex } = this.state;
     return (
       <View style={styles.container}>
-        <Text style={styles.headerText}>
-          Custom segmented control with custom styles
-        </Text>
-        <View style={styles.Seperator} />
-
-        {/* Simple Segmented with Custom Styling*/}
-        <SegmentedControlTab
-          values={["one", "two", "three"]}
-          selectedIndex={customStyleIndex}
-          onTabPress={this.handleCustomIndexSelect}
-          borderRadius={0}
-          tabsContainerStyle={{ height: 50, backgroundColor: "#F2F2F2" }}
-          tabStyle={{
-            backgroundColor: "#F2F2F2",
-            borderWidth: 0,
-            borderColor: "transparent"
-          }}
-          activeTabStyle={{ backgroundColor: "pink", marginTop: 2 }}
-          tabTextStyle={{ color: "navyblue", fontWeight: "bold" }}
-          activeTabTextStyle={{ color: "#888888" }}
+        <Spinner
+          //visibility of Overlay Loading Spinner
+          visible={this.state.loading}
+          //Text with the Spinner
+          textContent={"Loading..."}
+          //Text style of the Spinner Text
+          textStyle={styles.spinnerTextStyle}
         />
-        {customStyleIndex === 0 && (
-          <Text style={styles.tabContent}> Tab one Pressed</Text>
-        )}
-        {customStyleIndex === 1 && (
-          <Text style={styles.tabContent}> Tab two Pressed</Text>
-        )}
-        {customStyleIndex === 2 && (
-          <Text style={styles.tabContent}> Tab three Pressed</Text>
-        )}
+        <Text style={{ textAlign: "center", fontSize: 20 }}>
+          Spinner Overlay Example
+        </Text>
       </View>
     );
   }
@@ -64,37 +43,13 @@ class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
-    alignItems: "center",
-    backgroundColor: "green",
-
-    padding: 10
+    justifyContent: "center",
+    textAlign: "center",
+    paddingTop: 30,
+    backgroundColor: "#ecf0f1",
+    padding: 8
   },
-  headerText: {
-    padding: 8,
-    fontSize: 17,
-    color: "blue",
-    textAlign: "center"
-  },
-  tabContent: {
-    color: "red",
-    fontSize: 20,
-    margin: 24
-  },
-  Seperator: {
-    marginHorizontal: -10,
-    alignSelf: "stretch",
-    borderTopWidth: 1,
-    borderTopColor: "black",
-    margin: 15,
-    marginTop: 24
-  },
-  tabStyle: {
-    borderColor: "#D52C43"
-  },
-  activeTabStyle: {
-    backgroundColor: "#D52C43"
+  spinnerTextStyle: {
+    color: "#FFF"
   }
 });
-
-export default App;
