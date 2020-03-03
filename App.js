9352ca073e11ee08759d750b1a7219720476b502
Viren Asodariya <sc@SCs-Mac-mini.local>
2020-03-03 12:14:17 +0530

    typography support only ios example

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { material } from "react-native-typography";
+import { iOSColors } from "react-native-typography";
 
 export default class HelloWorldApp extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text style={material.display4}>Hello Material!</Text>
+        <Text style={iOSColors.display4}>Hello Material!</Text>
       </View>
     );
   }
