a1253260a9b8686d05d45e82b3d464b526c9ef89
Viren Asodariya <sc@SCs-Mac-mini.local>
2020-03-03 12:24:07 +0530

    all typograpy size in font example in android

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { iOSColors } from "react-native-typography";
+import { material } from "react-native-typography";
 
 export default class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text style={iOSColors.display4}>Hello Material!</Text>
+      <View>
+        <Text style={material.display4}>Hello Material!</Text>
+        <Text style={material.display3}>Reguler 56</Text>
+        <Text style={material.display2}>Reguler 45</Text>
+        <Text style={material.display1}>Reguler 34</Text>
+        <Text style={material.headline}>Reguler 24</Text>
+        <Text style={material.title}>medium 20</Text>
+        <Text style={material.subheading}>Reguler 16</Text>
+        <Text style={material.body2}>medium 14</Text>
+        <Text style={material.body1}>Reguler 14</Text>
+        <Text style={material.caption}>Reguler 12</Text>
+        <Text style={material.buttom}>medium 14</Text>
       </View>
     );
   }
