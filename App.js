df18603def97f591d585a2527a2f09d6a7fa5c3c
Viren Asodariya <sc@SCs-Mac-mini.local>
2020-03-12 12:01:28 +0530

    Drawer navigation simple example

--- App.js
+++ App.js
@@ -3,29 +3,51 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "./pages/HomeScreen";
 import AboutScreen from "./pages/AboutScreen";
 import ContactScreen from "./pages/ContactScreen";
 
-export default function App() {
-  const stack = createStackNavigator();
-
+function Home() {
   return (
-    <NavigationContainer>
-      <stack.Navigator>
-        <stack.Screen name="Home" component={HomeScreen} />
-        <stack.Screen name="About" component={AboutScreen} />
-        <stack.Screen name="Contact" component={ContactScreen} />
-      </stack.Navigator>
-    </NavigationContainer>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+function About() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>About Screen</Text>
+    </View>
+  );
+}
+
+function Contact() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Contact Screen</Text>
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="About" component={About} />
+      <Drawer.Screen name="Contact" component={Contact} />
+    </Drawer.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyDrawer />
+    </NavigationContainer>
+  );
+}
