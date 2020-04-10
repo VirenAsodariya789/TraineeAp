14c1da9de3acaa4114e3947ce28f32f2b392d42e
viren <asodariyaviren789@gmail.com>
2020-04-10 11:27:04 +0530

    scrolview pratice demo

--- App.js
+++ App.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,Image,ScrollView,TextInput } from 'react-native';
 
-export default class HelloWorldApp extends Component {
+export default class ScrollViewExample extends Component {
   render() {
     return (
-      <View style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center"
-        }}>
-        <Text>Hello, world!</Text>
-      </View>
+      <ScrollView>
+      
+        <View>
+          <Text>hello</Text>
+          <Text>H R U?</Text>
+          <Image source="https://reactnative.dev/docs/assets/p_cat2.png"
+            style={{width:200,height:200}}/>
+
+        </View>
+        <TextInput style={{height:80,borderRadius:10,borderColor:'red',borderWidth:1}}
+        defaultValue="you can type in"/>
+        <View>
+          <Text>hello</Text>
+          <Text>H R U?</Text>
+          <Text>hello</Text>
+          <Text>H R U?</Text>
+          <Text>hello</Text>
+          <Text>H R U?</Text>
+          <Text>hello</Text>
+          <Text>H R U?</Text>
+          <Text>hello</Text>
+          <Text>H R U?</Text>
+          <Text>hello</Text>
+          <Text>H R U?</Text>
+          <Text>hello</Text>
+          <Text>H R U?</Text>
+          <Text>hello</Text>
+          <Text>H R U?</Text>
+          <Text>hello</Text>
+          <Text>H R U?</Text>
+          </View>
+      </ScrollView>
     );
   }
 }
