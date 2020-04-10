b534ecca08b01fd69cfa6723748141ed8931e5b0
viren <asodariyaviren789@gmail.com>
2020-04-10 11:09:46 +0530

    pratice in hello world

--- App.js
+++ App.js
@@ -1,43 +1,16 @@
 import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 
-import { StyleSheet, View, Text, ScrollView, Platform, Alert,Animated } from 'react-native';
- 
-class MyApp extends React.Component
-{
-  constructor(props){
-    super(props);
-    this.position=new Animated.ValueXY(0,0);
-    Animated.spring(this.position,{
-      toValue:{x:200,y:300},
-      stiffness:300
-    }).start()
-  }
-  
-  render()
-  {
-    return(
-      <Animated.View style={this.position.getLayout()}>
-        <View style={styles.ball}/>
-      </Animated.View>
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+      <View style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center"
+        }}>
+        <Text>Hello, world!</Text>
+      </View>
     );
   }
 }
- export default MyApp
-const styles = StyleSheet.create(
-{
-  container:
-  {
-    flex: 1,
-    backgroundColor:'#fff',
-  
-    
-  },
-  ball:{
-    width:60,
-    height:60,
-    borderRadius:30,
-    backgroundColor:'red',
-  }
-
-
-});
