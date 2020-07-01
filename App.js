df476f5cecafe6ec98c74d1257ce64ddc0857eac
Viren Asodariya <sc@SCs-Mac-mini.local>
2020-07-01 11:33:54 +0530

    reactnative Appstate-active and background

--- App.js
+++ App.js
@@ -1,64 +1,42 @@
-import React, { useRef } from "react";
-import { Animated, Text, View, StyleSheet, Button } from "react-native";
-
-export default function App() {
-  // fadeAnim will be used as the value for opacity. Initial Value: 0
-  const fadeAnim = useRef(new Animated.Value(0)).current;
-
-  const fadeIn = () => {
-    // Will change fadeAnim value to 1 in 5 seconds
-    Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: 2000,
-    }).start();
+//This is an example code for AppState//
+import React, { Component } from "react";
+//import react in our code.
+import { AppState, Text } from "react-native";
+//import all the components we are going to use.
+export default class App extends Component {
+  state = {
+    appState: AppState.currentState,
   };
-
-  const fadeOut = () => {
-    // Will change fadeAnim value to 0 in 5 seconds
-    Animated.timing(fadeAnim, {
-      toValue: 0,
-      duration: 2000,
-    }).start();
+  componentDidMount() {
+    AppState.addEventListener("change", this._handleAppStateChange);
+  }
+  componentWillUnmount() {
+    AppState.removeEventListener("change", this._handleAppStateChange);
+  }
+  _handleAppStateChange = (nextAppState) => {
+    if (
+      this.state.appState.match(/inactive|background/) &&
+      nextAppState === "active"
+    ) {
+      console.log("App State: " + "App has come to the foreground!");
+      alert("App State: " + "App has come to the foreground!");
+    }
+    console.log("App State: " + nextAppState);
+    alert("App State: " + nextAppState);
+    this.setState({ appState: nextAppState });
   };
-
-  return (
-    <View style={styles.container}>
-      <Animated.View
-        style={[
-          styles.fadingContainer,
-          {
-            opacity: fadeAnim, // Bind opacity to animated value
-          },
-        ]}
+  render() {
+    return (
+      <Text
+        style={{
+          flex: 1,
+          marginTop: 30,
+          padding: 20,
+          backgroundColor: "white",
+        }}
       >
-        <Text style={styles.fadingText}>Fading View!</Text>
-      </Animated.View>
-      <View style={styles.buttonRow}>
-        <Button title="Fade In" onPress={fadeIn} />
-        <Button title="Fade Out" onPress={fadeOut} />
-      </View>
-    </View>
-  );
+        Current state is: {this.state.appState}
+      </Text>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  fadingContainer: {
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    backgroundColor: "powderblue",
-  },
-  fadingText: {
-    fontSize: 28,
-    textAlign: "center",
-    margin: 10,
-  },
-  buttonRow: {
-    flexDirection: "row",
-    marginVertical: 16,
-  },
-});
