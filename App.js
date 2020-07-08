6146f5bfba621e06b51491785d2ff337fce99662
Viren Asodariya <sc@SCs-Mac-mini.local>
2020-07-08 11:05:18 +0530

    keyboard tying in screen example

--- App.js
+++ App.js
@@ -1,124 +1,58 @@
 import React, { Component } from "react";
 import {
+  View,
+  KeyboardAvoidingView,
+  TextInput,
   StyleSheet,
   Text,
-  View,
-  Easing,
-  Animated,
-  ScrollView,
+  Platform,
+  TouchableWithoutFeedback,
+  Button,
+  Keyboard,
 } from "react-native";
-import Button from "react-native-button";
 
-export default class App extends Component {
-  constructor() {
-    super();
-    this.animatedValue = new Animated.Value(0);
-  }
+const KeyboardAvoidingComponent = () => {
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.inner}>
+          <Text style={styles.header}>Header</Text>
+          <TextInput placeholder="Username" style={styles.textInput} />
+          <View style={styles.btnContainer}>
+            <Button title="Submit" onPress={() => null} />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+};
 
-  animate(easing) {
-    this.animatedValue.setValue(0);
-    Animated.timing(this.animatedValue, {
-      toValue: 1,
-      duration: 1000,
-      easing,
-    }).start();
-  }
-
-  render() {
-    const marginLeft = this.animatedValue.interpolate({
-      inputRange: [0, 100],
-      outputRange: [0, 400],
-    });
-    return (
-      <View style={styles.container}>
-        <Animated.View style={[styles.block, { marginLeft }]} />
-        <ScrollView>
-          <Text style={{ textAlign: "center" }}>
-            Scroll up for more animations
-          </Text>
-          <MyButton
-            style={styles.button}
-            title="Bounce"
-            onPress={this.animate.bind(this, Easing.bounce)}
-          />
-          <MyButton
-            style={styles.button}
-            title="Cubic"
-            onPress={this.animate.bind(this, Easing.cubic)}
-          />
-          <MyButton
-            style={styles.button}
-            title="Back"
-            onPress={this.animate.bind(this, Easing.back(2))}
-          />
-          <MyButton
-            style={styles.button}
-            title="Elastic"
-            onPress={this.animate.bind(this, Easing.elastic(2))}
-          />
-          <MyButton
-            style={styles.button}
-            title="Ease"
-            onPress={this.animate.bind(this, Easing.ease)}
-          />
-          <MyButton
-            style={styles.button}
-            title="InOut"
-            onPress={this.animate.bind(this, Easing.inOut(Easing.quad))}
-          />
-          <MyButton
-            style={styles.button}
-            title="In"
-            onPress={this.animate.bind(this, Easing.in(Easing.quad))}
-          />
-          <MyButton
-            style={styles.button}
-            title="Out"
-            onPress={this.animate.bind(this, Easing.out(Easing.quad))}
-          />
-          <MyButton
-            style={styles.button}
-            title="Sin"
-            onPress={this.animate.bind(this, Easing.sin)}
-          />
-          <MyButton
-            style={styles.button}
-            title="Linear"
-            onPress={this.animate.bind(this, Easing.linear)}
-          />
-          <MyButton
-            style={styles.button}
-            title="Quad"
-            onPress={this.animate.bind(this, Easing.quad)}
-          />
-        </ScrollView>
-      </View>
-    );
-  }
-}
-
-const MyButton = ({ onPress, title, style }) => (
-  <Button style={style} onPress={onPress}>
-    {title}
-  </Button>
-);
-
-var styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 60,
   },
-  button: {
-    height: 60,
-    backgroundColor: "#ededed",
-    borderRadius: 4,
-    marginTop: 10,
-    paddingTop: 20,
-    fontSize: 18,
+  inner: {
+    padding: 24,
+    flex: 1,
+    justifyContent: "space-around",
   },
-  block: {
-    width: 50,
-    height: 50,
-    backgroundColor: "blue",
+  header: {
+    fontSize: 36,
+    marginBottom: 48,
+  },
+  textInput: {
+    height: 40,
+    borderColor: "#000000",
+    borderBottomWidth: 1,
+    marginBottom: 36,
+  },
+  btnContainer: {
+    backgroundColor: "white",
+    marginTop: 12,
   },
 });
+
+export default KeyboardAvoidingComponent;
