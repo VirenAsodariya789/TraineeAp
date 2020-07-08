f73d1436fe39319912dd79fdddbe2e5de295f695
Viren Asodariya <sc@SCs-Mac-mini.local>
2020-07-08 10:37:50 +0530

    backhanler example

--- App.js
+++ App.js
@@ -1,50 +1,63 @@
-//This is an example code for AppState//
 import React, { Component } from "react";
-//import react in our code.
-import { StatusBar } from "react-native";
-import { Text, AsyncStorage, View, TextInput, StyleSheet } from "react-native";
-import { Value } from "react-native-reanimated";
-//import all the components we are going to use.
-class AsyncStorageExample extends Component {
-  state = {
-    name: "",
-  };
-  componentDidMount = () =>
-    AsyncStorage.getItem("name").then((Value) =>
-      this.setState({ name: Value })
-    );
 
-  setName = (Value) => {
-    AsyncStorage.setItem("name", Value);
-    this.setState({ name: Value });
+import {
+  Text,
+  View,
+  StyleSheet,
+  Platform,
+  Alert,
+  BackHandler,
+} from "react-native";
+
+export default class App extends Component {
+  componentDidMount() {
+    BackHandler.addEventListener("hardwareBackPress", this.onBackPress);
+  }
+
+  componentWillMount() {
+    BackHandler.removeEventListener("hardwareBackPress", this.onBackPress);
+  }
+
+  onBackPress = () => {
+    Alert.alert(
+      "Exit from App",
+      "Do you want to exit from App?",
+      [
+        {
+          text: "Yes",
+          onBackPress: () => BackHandler.exitApp(),
+        },
+        {
+          text: "No",
+          onBackPress: () => console.log("NO Pressed"),
+        },
+      ],
+      { cancelable: false }
+    );
+    return true;
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <TextInput
-          style={styles.textInput}
-          autoCapitalize="none"
-          onChangeText={this.setName}
-        />
-        <Text>{this.state.name}</Text>
+        <Text style={styles.headerText}>
+          Press Hardware back button and see the alert message{"\n"}
+        </Text>
       </View>
     );
   }
 }
 
-export default AsyncStorageExample;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
     alignItems: "center",
-    marginTop: 50,
   },
-  textInput: {
-    margin: 5,
-    height: 100,
-    width: 400,
-    borderWidth: 1,
-    backgroundColor: "#7685ed",
+  headerText: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+    fontWeight: "bold",
   },
 });
