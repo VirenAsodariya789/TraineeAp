905c762883c4253a33f994ddb36fdcbf3e20b3a8
viren <asodariyaviren789@gmail.com>
2020-07-23 10:44:27 +0530

    statusbar example

--- App.js
+++ App.js
@@ -1,58 +1,20 @@
-import React, { Component } from "react";
-import {
-  View,
-  KeyboardAvoidingView,
-  TextInput,
-  StyleSheet,
-  Text,
-  Platform,
-  TouchableWithoutFeedback,
-  Button,
-  Keyboard,
-} from "react-native";
+import React from 'react';
+import { StyleSheet, StatusBar, View } from 'react-native';
 
-const KeyboardAvoidingComponent = () => {
-  return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS == "ios" ? "padding" : "height"}
-      style={styles.container}
-    >
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.inner}>
-          <Text style={styles.header}>Header</Text>
-          <TextInput placeholder="Username" style={styles.textInput} />
-          <View style={styles.btnContainer}>
-            <Button title="Submit" onPress={() => null} />
-          </View>
+const AppStatusBar = ({backgroundColor, ...props}) => {
+    return (
+        <View style={[styles.statusBar, backgroundColor]}>
+            <StatusBar backgroundColor={backgroundColor} {...props} />
         </View>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
-  );
+    );
 };
 
+const BAR_HEIGHT = StatusBar.currentHeight;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  inner: {
-    padding: 24,
-    flex: 1,
-    justifyContent: "space-around",
-  },
-  header: {
-    fontSize: 36,
-    marginBottom: 48,
-  },
-  textInput: {
-    height: 40,
-    borderColor: "#000000",
-    borderBottomWidth: 1,
-    marginBottom: 36,
-  },
-  btnContainer: {
-    backgroundColor: "white",
-    marginTop: 12,
-  },
+    statusBar: {
+        height: BAR_HEIGHT
+    },
 });
 
-export default KeyboardAvoidingComponent;
+export default AppStatusBar;
