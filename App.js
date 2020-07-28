ef625921b5969c038f1e790390357c0f518c6c51
viren <asodariyaviren789@gmail.com>
2020-07-28 12:20:32 +0530

    navigation header button

--- App.js
+++ App.js
@@ -1,25 +1,47 @@
-import React, { Component } from 'react'  
-import {  
-    View,StyleSheet,AppRegistry,StatusBar  
-} from 'react-native'  
-  
-export default class ActivityIndicatorDemo extends Component {  
-    render() {  
-        return (  
-            <View style = {styles.container}>  
-                <StatusBar  
-                    backgroundColor = "#b3e6ff"  
-                    barStyle = "dark-content"   
-                    hidden = {false}    
-                    translucent = {true}  
-                />  
-            </View>  
-        )  
-    }  
-}  
-const styles = StyleSheet.create({  
-    container: {  
-        flex: 1,  
-    }  
-})  
-AppRegistry.registerComponent('App', () => ActivityIndicatorDemo)  
+import * as React from 'react';
+import { Button, Text, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function LogoTitle() {
+  return (
+    <Image
+      style={{ width: 50, height: 50 }}
+      
+    />
+  );
+}
+
+function HomeScreen({ navigation }) {
+  const [count, setCount] = React.useState(0);
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Button onPress={() => setCount(c => c + 1)} title="Update count" />
+      ),
+    });
+  }, [navigation, setCount]);
+
+  return <Text>Count: {count}</Text>;
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={({ navigation, route }) => ({
+            headerTitle: props => <LogoTitle {...props} />,
+          })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
