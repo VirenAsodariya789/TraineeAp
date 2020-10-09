c58537844ab3e3b15ccb02cfe2817c956e58f915
Viren Asodariya <sc@SCs-Mac-mini.local>
2020-10-09 11:51:59 +0530

    get and post data fetch in hhtp api

--- App.js
+++ App.js
@@ -1,47 +1,119 @@
-import React, { Component } from "react";
-import { ActivityIndicator, FlatList, Text, View } from "react-native";
+// React Native Fetch – To make HTTP API call in React Native
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
+// import React in our code
+import React from "react";
 
-    this.state = {
-      data: [],
-      isLoading: true,
-    };
-  }
+// import all the components we are going to use
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 
-  componentDidMount() {
-    fetch("https://reactnative.dev/movies.json")
+const App = () => {
+  const getDataUsingGet = () => {
+    //GET request
+    fetch("https://jsonplaceholder.typicode.com/posts/1", {
+      method: "GET",
+      //Request Type
+    })
       .then((response) => response.json())
-      .then((json) => {
-        this.setState({ data: json.movies });
+      //If response is in json then in success
+      .then((responseJson) => {
+        //Success
+        alert(JSON.stringify(responseJson));
+        console.log(responseJson);
       })
-      .catch((error) => console.error(error))
-      .finally(() => {
-        this.setState({ isLoading: false });
+      //If response is not in json then in error
+      .catch((error) => {
+        //Error
+        alert(JSON.stringify(error));
+        console.error(error);
       });
-  }
+  };
 
-  render() {
-    const { data, isLoading } = this.state;
+  const getDataUsingPost = () => {
+    //POST json
+    var dataToSend = { title: "foo", body: "bar", userId: 1 };
+    //making data to send on server
+    var formBody = [];
+    for (var key in dataToSend) {
+      var encodedKey = encodeURIComponent(key);
+      var encodedValue = encodeURIComponent(dataToSend[key]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+    //POST request
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST", //Request Type
+      body: formBody, //post body
+      headers: {
+        //Header Defination
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      //If response is in json then in success
+      .then((responseJson) => {
+        alert(JSON.stringify(responseJson));
+        console.log(responseJson);
+      })
+      //If response is not in json then in error
+      .catch((error) => {
+        alert(JSON.stringify(error));
+        console.error(error);
+      });
+  };
 
-    return (
-      <View style={{ flex: 1, padding: 24 }}>
-        {isLoading ? (
-          <ActivityIndicator />
-        ) : (
-          <FlatList
-            data={data}
-            keyExtractor={({ id }, index) => id}
-            renderItem={({ item }) => (
-              <Text>
-                {item.title}, {item.releaseYear}
-              </Text>
-            )}
-          />
-        )}
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <View style={styles.container}>
+          {/*Running GET Request*/}
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            onPress={getDataUsingGet}
+          >
+            <Text style={styles.textStyle}>Get Data Using GET</Text>
+          </TouchableOpacity>
+          {/*Running POST Request*/}
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            onPress={getDataUsingPost}
+          >
+            <Text style={styles.textStyle}>Get Data Using POST</Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={{ fontSize: 18, textAlign: "center", color: "grey" }}>
+          Ask Run Time Android Permission{"\n"}React Navigation
+        </Text>
+        <Text style={{ fontSize: 16, textAlign: "center", color: "grey" }}>
+          www.google.com
+        </Text>
       </View>
-    );
-  }
-}
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    justifyContent: "center",
+    padding: 20,
+  },
+  textStyle: {
+    fontSize: 18,
+    color: "white",
+  },
+  buttonStyle: {
+    alignItems: "center",
+    backgroundColor: "#f4511e",
+    padding: 10,
+    marginVertical: 10,
+  },
+});
+
+export default App;
