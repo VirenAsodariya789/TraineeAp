fe0f97889501ff36111cb3045b2d48fce85e26cb
Viren Asodariya <sc@SCs-Mac-mini.local>
2020-12-01 11:47:54 +0530

    image picker demo

--- App.js
+++ App.js
@@ -1,20 +1,103 @@
-import React, { Component } from "react";
-import { View, StyleSheet, AppRegistry } from "react-native";
-import { WebView } from "react-native-webview";
+import React from "react";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import ImagePicker from "react-native-image-picker";
 
-export default class ActivityIndicatorDemo extends Component {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      resourcePath: {},
+    };
+  }
+
+  selectFile = () => {
+    var options = {
+      title: "Select Image",
+      customButtons: [
+        {
+          name: "customOptionKey",
+          title: "choose file from custom option",
+        },
+      ],
+      storageOption: {
+        skipBackup: true,
+        path: "images",
+      },
+    };
+
+    ImagePicker.showImagePicker(options, (res) => {
+      console.log("Response = ", res);
+
+      if (res.didCancel) {
+        console.log("User cancelled image picker");
+      } else if (res.error) {
+        console.log("ImagePicker Error: ", res.error);
+      } else if (res.customButton) {
+        console.log("User tapped custom button: ", res.customButton);
+        alert(res.customButton);
+      } else {
+        let source = res;
+        this.setState({
+          resourcePath: source,
+        });
+      }
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <WebView source={{ uri: "https://www.google.com" }} />
+        <View style={styles.container}>
+          <Image
+            source={{
+              uri: "data:image/jpeg;base64," + this.state.resourcePath.data,
+            }}
+            style={{ width: 100, height: 100 }}
+          />
+          <Image
+            source={{ uri: this.state.resourcePath.uri }}
+            style={{ width: 200, height: 200 }}
+          />
+          <Text style={{ alignItems: "center" }}>
+            {this.state.resourcePath.uri}
+          </Text>
+
+          <TouchableOpacity onPress={this.selectFile} style={styles.button}>
+            <Text style={styles.buttonText}>Select File</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  button: {
+    width: 250,
+    height: 60,
+    backgroundColor: "#3740ff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 4,
+    marginBottom: 12,
+  },
+  buttonText: {
+    textAlign: "center",
+    fontSize: 15,
+    color: "#fff",
   },
 });
-
-AppRegistry.registerComponent("App", () => ActivityIndicatorDemo);
