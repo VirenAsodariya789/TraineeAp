b559985b8ed68f034cd3a097aa3bea30c27c3a44
Viren Asodariya <sc@SCs-Mac-mini.local>
2020-12-02 11:53:08 +0530

    reader in mapview

--- App.js
+++ App.js
@@ -7,97 +7,20 @@ import {
   TouchableOpacity,
   Image,
 } from "react-native";
-import ImagePicker from "react-native-image-picker";
+import MapView from "react-native-maps";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      resourcePath: {},
-    };
-  }
+const App = () => {
+  return (
+    <MapView
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05,
+      }}
+    />
+  );
+};
 
-  selectFile = () => {
-    var options = {
-      title: "Select Image",
-      customButtons: [
-        {
-          name: "customOptionKey",
-          title: "choose file from custom option",
-        },
-      ],
-      storageOption: {
-        skipBackup: true,
-        path: "images",
-      },
-    };
-
-    ImagePicker.showImagePicker(options, (res) => {
-      console.log("Response = ", res);
-
-      if (res.didCancel) {
-        console.log("User cancelled image picker");
-      } else if (res.error) {
-        console.log("ImagePicker Error: ", res.error);
-      } else if (res.customButton) {
-        console.log("User tapped custom button: ", res.customButton);
-        alert(res.customButton);
-      } else {
-        let source = res;
-        this.setState({
-          resourcePath: source,
-        });
-      }
-    });
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.container}>
-          <Image
-            source={{
-              uri: "data:image/jpeg;base64," + this.state.resourcePath.data,
-            }}
-            style={{ width: 100, height: 100 }}
-          />
-          <Image
-            source={{ uri: this.state.resourcePath.uri }}
-            style={{ width: 200, height: 200 }}
-          />
-          <Text style={{ alignItems: "center" }}>
-            {this.state.resourcePath.uri}
-          </Text>
-
-          <TouchableOpacity onPress={this.selectFile} style={styles.button}>
-            <Text style={styles.buttonText}>Select File</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 30,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#fff",
-  },
-  button: {
-    width: 250,
-    height: 60,
-    backgroundColor: "#3740ff",
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 4,
-    marginBottom: 12,
-  },
-  buttonText: {
-    textAlign: "center",
-    fontSize: 15,
-    color: "#fff",
-  },
-});
+export default App;
